d3c309118a62298cb100fd3439c4b9ccbd5009dc
LiberVance <libervance@LiberVanceui-MacBookPro.local>
2023-02-24 12:00:22 +0900

    actions test

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Computes a factorial of a given positive integer
+function factorial(n){
+    if (n<0){
+        throw new Error('Factorial is only defined for non-negative integers!');
+    }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    if(n===0){
+        return 1;
+    }
+    return n * factorial(n-1);
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+module.exports = factorial;
